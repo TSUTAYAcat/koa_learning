3640a568ed0aab93e10efdb5e9fc978fc219f0c3
Your Name <you@example.com>
2020-04-06 14:27:38 +0800

    跨域

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 const Koa = require('koa');
 const app = new Koa();
 const router = require('./router');
+const cors = require('koa-cors');
 
+app.use(async (ctx, next)=> {
+    ctx.set('Access-Control-Allow-Origin', '*');
+    ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    if (ctx.method == 'OPTIONS') {
+      ctx.body = 200; 
+    } else {
+      await next();
+    }
+  });
 app.use(router.routes())
 app.listen(3000)
 
